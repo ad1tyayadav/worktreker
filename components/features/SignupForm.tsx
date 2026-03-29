98786576e515ccd9801bb09998fd1e54899aa75c
ad1tyayadav <adityaproworks@gmail.com>
2026-03-30 05:06:13 +0530

    fix: google Oauth

--- components/features/SignupForm.tsx
+++ components/features/SignupForm.tsx
@@ -29,7 +29,7 @@ export const SignupForm = () => {
       await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
-          redirectTo: `${window.location.origin}/auth/callback`,
+          redirectTo: `https://worktreker.online`,
         },
       });
     } catch {
